3a2119f3daddff9fef3ec5460c9604d1f48f7a69
Hein Bekker <hein@siyavula.com>
2017-11-08 06:28:39 +0200

    Add postinstall for datasets

--- datasets/install.sh
+++ datasets/install.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+cyan=`tput setaf 6`
+reset=`tput sgr0`
+
+function download () {
+  local SRC=$1
+  local DEST=$2
+
+  if [ ! -f $DEST ]; then
+    wget -O "$DEST" "$SRC"
+  fi
+}
+
 # London Air Quality Network
-# http://api.erg.kcl.ac.uk/AirQuality/help/operations/GetRawDataSiteCsv
-mkdir -p laqn
-cd laqn
-wget -O bq7-20160101-20161231.csv http://api.erg.kcl.ac.uk/AirQuality/Data/Site/Wide/SiteCode=BQ7/StartDate=1-jan-2016/EndDate=31-dec-2016/csv
-wget -O hk6-20160101-20161231.csv http://api.erg.kcl.ac.uk/AirQuality/Data/Site/Wide/SiteCode=HK6/StartDate=1-jan-2016/EndDate=31-dec-2016/csv
-wget -O my1-20160101-20161231.csv http://api.erg.kcl.ac.uk/AirQuality/Data/Site/Wide/SiteCode=MY1/StartDate=1-jan-2016/EndDate=31-dec-2016/csv
+# API: http://api.erg.kcl.ac.uk/AirQuality/help/operations/GetRawDataSiteCsv
+# Terms and conditions: http://api.erg.kcl.ac.uk/AirQuality/Information/Terms/pdf
+DIR="laqn"
+printf "Starting '${cyan}$DIR${reset}'...\n"
+mkdir -p "$DIR"
+download "http://api.erg.kcl.ac.uk/AirQuality/Data/Site/Wide/SiteCode=BQ7/StartDate=1-jan-2016/EndDate=31-dec-2016/csv" "$DIR/bq7-20160101-20161231.csv"
+download "http://api.erg.kcl.ac.uk/AirQuality/Data/Site/Wide/SiteCode=HK6/StartDate=1-jan-2016/EndDate=31-dec-2016/csv" "$DIR/hk6-20160101-20161231.csv"
+download "http://api.erg.kcl.ac.uk/AirQuality/Data/Site/Wide/SiteCode=MY1/StartDate=1-jan-2016/EndDate=31-dec-2016/csv" "$DIR/my1-20160101-20161231.csv"
+printf "Finished '${cyan}$DIR${reset}'\n"
