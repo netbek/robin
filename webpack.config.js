6a1837683998fd920bd61c2c13dfab2443dac8f4
Hein Bekker <hein@netbek.co.za>
2018-04-12 03:55:29 +0200

    Tweak webpack config for ES5 output

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,43 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules\/(?!(react-redux-form))/, // Exclude dirs in node_modules except react-redux-form
+        // We intentionally transpile everything except modules that meet the
+        // following criteria: 1) the module breaks transpilation 2) the module
+        // has ES5 compatible code. This defensive approach ensures that we have
+        // ES5 output. The list could be extended in order to reduce build time.
+        exclude: [
+          './node_modules/@vx/annotation',
+          './node_modules/@vx/axis',
+          './node_modules/@vx/bounds',
+          './node_modules/@vx/boxplot',
+          './node_modules/@vx/brush',
+          './node_modules/@vx/clip-path',
+          './node_modules/@vx/curve',
+          './node_modules/@vx/demo',
+          './node_modules/@vx/drag',
+          './node_modules/@vx/event',
+          './node_modules/@vx/geo',
+          './node_modules/@vx/glyph',
+          './node_modules/@vx/gradient',
+          './node_modules/@vx/grid',
+          './node_modules/@vx/group',
+          './node_modules/@vx/heatmap',
+          './node_modules/@vx/hierarchy',
+          './node_modules/@vx/legend',
+          './node_modules/@vx/marker',
+          './node_modules/@vx/mock-data',
+          './node_modules/@vx/network',
+          './node_modules/@vx/pattern',
+          './node_modules/@vx/point',
+          './node_modules/@vx/responsive',
+          './node_modules/@vx/scale',
+          './node_modules/@vx/shape',
+          './node_modules/@vx/text',
+          './node_modules/@vx/tooltip',
+          './node_modules/@vx/voronoi',
+          './node_modules/@vx/vx',
+          './node_modules/@vx/zoom'
+        ].map(d => path.resolve(d)),
         loader: 'babel-loader?cacheDirectory'
       }
     ]
